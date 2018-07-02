6cb9f3f757887a352bc1564b623786223fae6d70
DonaldPeat <don@donaldpeat.com>
2018-07-01 19:29:38 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vue-webpack-starter
+https://stackoverflow.com/questions/51127176/unexpected-token-import-error-while-running-jest-tests
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+<script>
+
+import 'vue-awesome/icons/trash'
+
+import Icon from 'vue-awesome/components/Icon.vue'
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+h1
+  color red
+</style>
